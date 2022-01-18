38c28cc44e715a6208b58d3e7d47ccdce0d2d8e1
Nikolay_Bondarenko <Nikolayb33@MacBook-Air-mac.local>
2022-01-18 21:54:11 +0300

    change first and second task

--- Bondarenko_Nikolai_hw6.sql
+++ Bondarenko_Nikolai_hw6.sql
@@ -1,34 +1,25 @@
 use vk;
 -- 1) пусть задан пользователь, из всех друзей найдите, кто больше всего с ним общался
+-- указал получателя
 
--- 1.1) кто из друзей больше всего отправил сообщений 
-
-SELECT from_user_id, count(id) FROM messages WHERE from_user_id IN ( 
+SELECT from_user_id, count(id), to_user_id FROM messages WHERE from_user_id IN ( 
  SELECT initiator_user_id FROM friend_requests WHERE (target_user_id = 1) AND status='approved' -- ИД друзей, заявку которых я подтвердил
  union
  SELECT target_user_id FROM friend_requests WHERE (initiator_user_id = 1) AND status='approved' -- ИД друзей, подтвердивших мою заявку
-)
+) and to_user_id = 1
 group by from_user_id
 order by count(id) desc;
 
--- 1.2) кто из друзей больше всего получил сообщений
-
-SELECT to_user_id , count(id) FROM messages WHERE to_user_id IN (  
- SELECT initiator_user_id FROM friend_requests WHERE (target_user_id = 1) AND status='approved' -- ИД друзей, заявку которых я подтвердил
- union
- SELECT target_user_id FROM friend_requests WHERE (initiator_user_id = 1) AND status='approved' -- ИД друзей, подтвердивших мою заявку
-)
-group by to_user_id
-order by count(id) desc;
 
 -- 2) подсчитайте общее количество лайков, которые получили пользователи младше 11 лет
+-- исправил, нашел кто лайкает 
 
-select
-  count(id) as younger_11
-  from likes
-  where user_id in 
-    (select id from users where 
-      (select user_id from profiles where user_id = users.id and timestampdiff(YEAR, birthday, NOW()) < 11));
+select count(id) as younger_11
+from likes
+where media_id in 
+  (select id from media where user_id in
+    (select id from users where id in
+      (SELECT user_id from profiles where user_id = users.id and timestampdiff(YEAR, birthday, NOW()) < 11)));
 
 -- 3) посчитать кто больше всего поставил лайков, мужчины или женщины
  
